ea1b7f5606e61a2398e1c100918273dbd922b938
Bohdan Robota <newbusines123@gmail.com>
2022-02-04 00:34:25 +0200

    clear master branch

--- aggregations/index.ts
+++
@@ -1,4 +0,0 @@
-export { returnStatesPipeline } from './returnStates.aggregation';
-export { returnCitiesPipeline } from './returnCities.aggregation';
-export { returnStatesArrPipeline } from './returnStatesArr.aggregation';
-export { returnFirst10CitiesPipeline } from './returnFirst10Cities.aggregation';
